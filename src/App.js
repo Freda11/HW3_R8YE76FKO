c1bd0e59606b3a7ec6569958b693c13f1250520a
Freda11 <ofreda564@gmail.com>
2020-09-03 18:24:51 +0000

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Component/Nav'
+import Content from './Component/Content'
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+function Nav() {
+    return(<div>
+        {/*<!-- Navbar -->*/}
+    <nav class="navbar">
+      <div class="container">
+        <a href="#">Hulk</a>
+       { /*<!-- Currently Active -->*/}
+        <a href="#" class="logo is-active">Mavel</a>
+        <a href="#">Venom</a>
+      </div>
+    </nav>
+
+    </div>)
+    
+}
+export default Nav
